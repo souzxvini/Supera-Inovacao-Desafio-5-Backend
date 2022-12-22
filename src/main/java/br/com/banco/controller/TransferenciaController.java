23296451fb7f77b01434aba84b52b942c4f98862
thesouzx <vnsoliveira2512@gmail.com>
2022-12-22 12:12:23 -0300

    desafio 100%

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/transferencia")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class TransferenciaController {
 
     @Autowired
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,18 +29,32 @@ INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_tra
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (2,'2019-02-03 09:53:27+03',12.24,'DEPOSITO', null,2);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (3,'2019-05-04 08:12:45+03',-500.50,'SAQUE', null,1);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (4,'2019-08-07 08:12:45+03',-530.50,'SAQUE', null,2);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (5,'2020-06-08 10:15:01+03',2241.23,'TRANSFERENCIA', 'Beltrano',1);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (6,'2021-04-01 12:12:04+03',25173.09,'TRANSFERENCIA', 'Ronnyscley',2);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (7,'2020-01-08 10:15:01+03',100.00,'DEPOSITO', null,3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (5,'2020-06-08 10:15:01+03',2241.23,'TRANSFERENCIA', 'Beltrano da Silva',1);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (6,'2021-04-01 12:12:04+03',25173.09,'TRANSFERENCIA', 'Ronnyscley Moreira Santos',2);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (7,'2020-01-08 10:15:45+03',100.00,'DEPOSITO', null,3);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (8,'2017-03-28 10:15:01+03',-1000.00,'SAQUE', null,4);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (9,'2015-05-01 10:15:01+03',670.00,'TRANSFERENCIA', 'Alan',5);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (10,'2022-05-11 10:15:01+03',3241.65,'TRANSFERENCIA', 'Marcos',2);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (9,'2015-05-01 10:15:58+03',670.00,'TRANSFERENCIA', 'Alan Oliveira Neto',5);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (10,'2022-05-11 09:58:26+03',3241.65,'TRANSFERENCIA', 'Marcos Silva',2);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (11, '2021-01-27 10:15:01+03',-3000.87,'SAQUE', null,5);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (12,'2022-09-13 10:15:01+03',876.47,'TRANSFERENCIA', 'Tatiana',2);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (12,'2022-09-13 18:22:11+03',876.47,'TRANSFERENCIA', 'Tatiana Foster',2);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (13,'2020-01-08 10:15:01+03',654.84,'DEPOSITO', null,4);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (14,'2020-01-08 10:15:01+03',654.84,'TRANSFERENCIA', 'Leandro',4);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (15,'2020-01-08 10:15:01+03',654.84,'TRANSFERENCIA', 'Marcos',3);
-INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (16,'2020-01-08 10:15:01+03',654.84,'DEPOSITO', null,3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (14,'2020-01-08 15:12:25+03',-333.99,'TRANSFERENCIA', 'Leandro de Almeida',4);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (15,'2020-01-08 10:15:01+03',654.84,'TRANSFERENCIA', 'Marcos Silva',3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (16,'2020-01-08 22:59:23+03',-2155.00,'DEPOSITO', null,3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (17,'2020-01-08 10:55:48+03',768.90,'TRANSFERENCIA', 'Leandro de Almeida',3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (18,'2020-01-08 01:15:01+03',10750.00,'DEPOSITO', null,4);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (19,'2020-01-08 03:21:01+03',12500.00,'TRANSFERENCIA', 'Beltrano da Silva',3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (20,'2020-01-08 20:27:01+03',-28000.00,'DEPOSITO', null,5);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (21,'2020-01-08 23:49:01+03',3000.00,'SAQUE', null,3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (22,'2020-01-08 19:15:01+03',-204.00,'TRANSFERENCIA', 'Beltrano da Silva',3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (23,'2020-01-08 11:25:01+03',654.84,'DEPOSITO', null,1);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (24,'2020-01-08 12:00:41+03',99.00,'TRANSFERENCIA', 'Leandro de Almeida',3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (25,'2020-01-08 18:50:01+03',-220.98,'SAQUE', null,1);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (26,'2020-01-08 19:15:51+03',560.89,'DEPOSITO', null,2);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (27,'2020-01-08 17:47:01+03',120.00,'SAQUE', null,3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (28,'2020-01-08 04:01:01+03',-1500.90,'TRANSFERENCIA', 'Alan Oliveira Neto',3);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (29,'2020-01-08 09:15:23+03',-750.87,'DEPOSITO', null,5);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (30,'2020-01-08 14:04:09+03',999.90,'TRANSFERENCIA', 'Tatiana Foster',3);
 
 
 
